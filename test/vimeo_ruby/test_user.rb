7a0a3494c5b48137c018fce7a5bcab9363db36f3
Collin Jilbert <cjilbert504@gmail.com>
2022-12-26 18:38:02 -0600

    breakup user attr tests

--- test/vimeo_ruby/test_user.rb
+++ test/vimeo_ruby/test_user.rb
@@ -4,16 +4,28 @@ require "test_helper"
 
 class TestUser < Minitest::Test
   def setup
-    VCR.insert_cassette(name)
+    VCR.use_cassette("user_found_successfully") do
+      @user = VimeoRuby::User.find(202558318)
+    end
   end
 
   def teardown
     VCR.eject_cassette
   end
 
-  def test_find_instantiates_a_user_after_successful_request
-    user = VimeoRuby::User.find(202558318)
-    assert_equal 202558318, user.id
-    assert_equal "Collin Jilbert", user.name
+  def test_user_has_an_id
+    assert_equal 202558318, @user.id
+  end
+
+  def test_user_has_a_name
+    assert_equal "Collin Jilbert", @user.name
+  end
+
+  def test_user_has_a_profile_link
+    assert_equal "https://vimeo.com/user202558318", @user.profile_url
+  end
+
+  def test_user_has_a_location
+    assert_equal "Covington, LA, USA", @user.location
   end
 end
