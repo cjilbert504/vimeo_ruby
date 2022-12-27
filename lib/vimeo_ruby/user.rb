1c5431fe05538965992b11d1519b400ce27f0370
Collin Jilbert <cjilbert504@gmail.com>
2022-12-26 23:30:06 -0600

    add more user attrs

--- lib/vimeo_ruby/user.rb
+++ lib/vimeo_ruby/user.rb
@@ -1,18 +1,28 @@
 module VimeoRuby
   class User < VimeoRuby::Base
-    attr_reader :id, :name, :profile_url, :location, :bio
+    attr_reader :vimeo_id, :name, :profile_url, :location, :bio, :available_for_hire
 
     def initialize(attrs: {})
-      @id = extract_id_from_vimeo_user_uri(attrs.delete("uri"))
+      @vimeo_id = extract_id_from_vimeo_user_uri(attrs.delete("uri"))
       @name = attrs["name"]
       @profile_url = attrs["link"]
       @location = attrs["location"]
       @bio = attrs["bio"]
+      @available_for_hire = attrs["available_for_hire"]
+      @can_work_remotely = attrs["can_work_remotely"]
     end
 
     def self.find(id)
       user_info = get("#{base_uri}/users/#{id}")
       new(attrs: user_info)
+    end
+
+    def available_for_hire?
+      @available_for_hire
+    end
+
+    def can_work_remotely?
+      @can_work_remotely
     end
 
     private
