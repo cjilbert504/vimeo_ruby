1da4001faf411d2052d07161e15f2be1686904de
Collin Jilbert <cjilbert504@gmail.com>
2022-12-27 21:46:38 -0600

    just require everything at the root lib file for now

--- lib/vimeo_ruby.rb
+++ lib/vimeo_ruby.rb
@@ -2,7 +2,9 @@
 
 require "vimeo_ruby/base"
 require "vimeo_ruby/user"
+require "vimeo_ruby/user/uploaded_video_collection"
 require "vimeo_ruby/version"
+require "vimeo_ruby/video"
 
 module VimeoRuby
   class Error < StandardError; end
