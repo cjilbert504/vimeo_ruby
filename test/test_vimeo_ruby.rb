ff248f80b273f7fe279ae25c8c938a1f3e80e25f
Collin Jilbert <cjilbert504@gmail.com>
2022-12-28 11:40:07 -0600

    add tests for new top level module shorthand methods for retrieving a user or video

--- test/test_vimeo_ruby.rb
+++ test/test_vimeo_ruby.rb
@@ -6,4 +6,30 @@ class TestVimeoRuby < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::VimeoRuby::VERSION
   end
+
+  def test_get_user
+    VCR.use_cassette("user_found_successfully") do
+      @vimeo_user1 = VimeoRuby.get_user(4111735)
+    end
+
+    VCR.use_cassette("user_found_successfully") do
+      @vimeo_user2 = VimeoRuby::User.get_user(4111735)
+    end
+
+    assert_equal @vimeo_user1.class, @vimeo_user2.class
+    assert_equal @vimeo_user1.vimeo_id, @vimeo_user2.vimeo_id
+  end
+
+  def test_get_video
+    VCR.use_cassette("video_found_successfully") do
+      @vimeo_video1 = VimeoRuby.get_video(697485076)
+    end
+
+    VCR.use_cassette("video_found_successfully") do
+      @vimeo_video2 = VimeoRuby::Video.get_video(697485076)
+    end
+
+    assert_equal @vimeo_video1.class, @vimeo_video2.class
+    assert_equal @vimeo_video1.vimeo_id, @vimeo_video2.vimeo_id
+  end
 end
