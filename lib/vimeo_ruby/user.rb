db67a5576fee43e685cbfabdc81473b54c3807a5
Collin Jilbert <cjilbert504@gmail.com>
2022-12-29 16:18:11 -0600

    initialize with access_token and send to parent via super; require access_token kwarg in get_user; pass access_token when retrieving a users uploaded videos

--- lib/vimeo_ruby/user.rb
+++ lib/vimeo_ruby/user.rb
@@ -2,7 +2,7 @@ module VimeoRuby
   class User < Base
     attr_reader :video_collection, :available_for_hire, :bio, :can_work_remotely, :location, :name, :profile_link
 
-    def initialize(attrs: {})
+    def initialize(access_token: nil, attrs: {})
       @available_for_hire = attrs.delete("available_for_hire")
       @bio = attrs.delete("bio")
       @can_work_remotely = attrs.delete("can_work_remotely")
@@ -11,13 +11,13 @@ module VimeoRuby
       @profile_link = attrs.delete("link")
       @video_collection = nil
       vimeo_uri_with_id = attrs.delete("uri")
-      super(vimeo_uri_with_id, attrs)
+      super(access_token: access_token, vimeo_id: vimeo_uri_with_id, remaining_attrs: attrs)
     end
 
     class << self
-      def get_user(user_id)
-        user_info = get("#{base_uri}/users/#{user_id}")
-        new(attrs: user_info)
+      def get_user(access_token: nil)
+        user_info = get("#{base_uri}/me", access_token: access_token)
+        new(access_token: access_token, attrs: user_info)
       end
     end
 
@@ -44,7 +44,8 @@ module VimeoRuby
     private
 
     def retrieve_video_collection(query_params)
-      uploaded_videos_response = self.class.get("#{base_uri}/users/#{vimeo_id}/videos", query_params: query_params)
+      uploaded_videos_response = self.class.get("#{base_uri}/me/videos", query_params: query_params, access_token: access_token)
+
       uploaded_videos = uploaded_videos_response["data"]
       UploadedVideoCollection.new(uploaded_videos)
     end
