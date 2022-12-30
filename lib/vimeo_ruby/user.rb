9cacc6d59b1fcdaf5c9ed958b8a17a461ef40e87
Collin Jilbert <cjilbert504@gmail.com>
2022-12-29 20:27:56 -0600

    organize methods; add video_feed method; fix class name reference

--- lib/vimeo_ruby/user.rb
+++ lib/vimeo_ruby/user.rb
@@ -21,8 +21,22 @@ module VimeoRuby
       end
     end
 
+    def available_for_hire?
+      @available_for_hire
+    end
+
     def base_uri
       self.class.base_uri
+    end
+
+    def can_work_remotely?
+      @can_work_remotely
+    end
+
+    def video_feed(query_params: {})
+      video_feed_response = self.class.get("#{base_uri}/me/feed", query_params: query_params, access_token: access_token)
+      video_feed = video_feed_response["data"]
+      VideoCollection.new(video_feed)
     end
 
     def uploaded_videos(query_params: {})
@@ -33,21 +47,13 @@ module VimeoRuby
       end
     end
 
-    def available_for_hire?
-      @available_for_hire
-    end
-
-    def can_work_remotely?
-      @can_work_remotely
-    end
-
     private
 
     def retrieve_video_collection(query_params)
       uploaded_videos_response = self.class.get("#{base_uri}/me/videos", query_params: query_params, access_token: access_token)
 
       uploaded_videos = uploaded_videos_response["data"]
-      UploadedVideoCollection.new(uploaded_videos)
+      VideoCollection.new(uploaded_videos)
     end
   end
 end
