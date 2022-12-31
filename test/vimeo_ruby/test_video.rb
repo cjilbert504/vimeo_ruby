729f8a5f589068dfe0805b74e75a9476823cd822
Collin Jilbert <cjilbert504@gmail.com>
2022-12-30 21:01:14 -0600

    use updated attr_reader

--- test/vimeo_ruby/test_video.rb
+++ test/vimeo_ruby/test_video.rb
@@ -31,7 +31,7 @@ class TestVideo < Minitest::Test
 
   def test_video_has_embed_html
     embed_html_str = "<iframe src=\"https://player.vimeo.com/video/697485076?h=545673a8c6&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=260485\" width=\"1920\" height=\"1080\" frameborder=\"0\" allow=\"autoplay; fullscreen; picture-in-picture\" allowfullscreen title=\"Ask Zillow Anything - Pilot\"></iframe>"
-    assert_equal embed_html_str, @video.embed_html
+    assert_equal embed_html_str, @video.embed
   end
 
   def test_video_has_a_link
@@ -52,9 +52,5 @@ class TestVideo < Minitest::Test
 
   def test_video_belongs_to_a_user
     assert_kind_of VimeoRuby::User, @video.user
-  end
-
-  def test_video_has_additional_info_accessible
-    assert_kind_of OpenStruct, @video.additional_info
   end
 end
